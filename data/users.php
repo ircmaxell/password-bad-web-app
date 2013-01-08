b36be851cf6e69d4d7418503f87d183b5ac8f8f5
Anthony Ferrara <ircmaxell@gmail.com>
2013-01-08 15:59:27 -0500

    Add encryption support on top of bcrypt

--- data/users.php
+++ data/users.php
@@ -3,30 +3,30 @@
   array (
     'id' => 1,
     'name' => 'foo',
-    'password' => '$2y$10$s2KNhP9.6Oln4nE4rfd5CuslDbwqE9dNYSGyGqHINSQemlHDAvoPS',
+    'password' => '60a82c1ac7e3513f41bb46fcef712f42f2f4e4bb6c55e7afd61068362353a5d4aGN382L4GUlHQkqe7VCDyByhzP9bZXb8HwCjlrU9XhmaU5KrwC2MT6Z07mMZkvKaU+x4NQ7VOzkDNqUqjLxD45WL++biwxkGHLGab4s7/8c=',
   ),
   1 => 
   array (
     'id' => 2,
     'name' => 'bar',
-    'password' => '$2y$10$Wrwi39JnXjCqDXx2XbRnweZrc8oC1xnhlxTzwJEnu1gwk3p.OSRJy',
+    'password' => 'a05edf9bd01f65f0cf29fcbb6afea1e62a639868659b9363f7cdfd0f384e7b744UF4vUsvdY2wgSV8KPKThaiiVWd4FDbd9KwfP96VGpvv2RO6Nbg6GQrMixGW72lRHYPA7WIhrCGcdOm57aAOVPMKw9Y/8R9LIV9ZPGaHl1U=',
   ),
   2 => 
   array (
     'id' => 3,
     'name' => 'baz',
-    'password' => '$2y$10$wU1lRXQNQoefVbG/gs76hOyuwMfnxM1WCwomMNiV4yUnhmjQocAky',
+    'password' => 'c946e33d3dfc02f55443ba22d0bf880adde7eead66cc703a94081e63bc919206X6ApCLevRt7anQ2KlVJbBhIGRzo2ReupbWIo9PaceP1YdynLA8kHQu/XOmjHgWXGx6GBHzWy0KirGOcC9N2jFWk4pu9NfxrTD4qJv1ti1KE=',
   ),
   3 => 
   array (
     'id' => 4,
     'name' => 'me',
-    'password' => '$2y$10$X9jUMxj6JkPtykSpMjPlqOOTNtFbOyUQpbayMwjsAl/ujFTrLPlOu',
+    'password' => 'bdf46bf4125cc123a06aa06cce368439fdaf4e6f3f17dba3bb29ec3d8e7218453y7j3U6yUSJwbbdq9JDVpfywkWdt/zHl3tbhYVjD043mmskFwTB55maZZpQ0z19O0nXzYduGiodP1yEVKlxJHshr34lRnFzjWtNwCa3Jxuk=',
   ),
   4 => 
   array (
     'name' => 'me2',
-    'password' => '$2y$10$1sIwTjT3Fq6cnT/4xm6iu.bVAO7kbnSovx99.HomcVzJJZHRj9Uxm',
+    'password' => 'ab22ddb67f7baa1f4fe30c3f91de55f5f5f7c9fc92dcfee1dcdb22a5d4a67bfcqjHKjwwIvpXlAZRORHAZd33zJJG2MiY8a38N8PzEBbiO767GB7MAD/xhLVNxHr6Pu8prAgT9fs4rvMKPQz80wFVhGUxPJwGyOVqg+u7qdcg=',
     'id' => 5,
   ),
 );
--- lib/password.php
+++ lib/password.php
@@ -1,10 +1,29 @@
 <?php
 
+use Zend\Crypt\BlockCipher;
+
+const CIPHER_KEY = "AVk05eoEmhgG10+s9D8VDXbJKGxAi67roAPRpeJRX4o49wfsg/o2FwuwU9dXk33cpWpcM1GcEUkIcLV+hMYPwg==";
+
 function createPassword($password) {
-    return password_hash($password, PASSWORD_DEFAULT);
+    return encrypt(password_hash($password, PASSWORD_DEFAULT));
 }
 
 function verifyPassword($hash, $password) {
-    return password_verify($password, $hash);
+    return password_verify($password, decrypt($hash));
 }
 
+function encrypt($password) {
+    global $key;
+    $cipher = BlockCipher::factory('mcrypt', array('algorithm' => 'aes'));
+    $cipher->setKey(base64_decode(CIPHER_KEY));
+
+    return $cipher->encrypt($password);
+}
+
+function decrypt($password) {
+    global $key;
+    $cipher = BlockCipher::factory('mcrypt', array('algorithm' => 'aes'));
+    $cipher->setKey(base64_decode(CIPHER_KEY));
+
+    return $cipher->decrypt($password);
+}
