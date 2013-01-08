28e279b9c8e92804a24dfb4dfb5beec5c54bc66e
Anthony Ferrara <ircmaxell@gmail.com>
2013-01-08 15:39:08 -0500

    Switch to password-compat

--- data/users.php
+++ data/users.php
@@ -3,30 +3,30 @@
   array (
     'id' => 1,
     'name' => 'foo',
-    'password' => '$2y$10$eb943fc3ad2d88b967030u2p2VAMTNU2Y9m0aIbvQ.YD/1IFozEAm',
+    'password' => '$2y$10$s2KNhP9.6Oln4nE4rfd5CuslDbwqE9dNYSGyGqHINSQemlHDAvoPS',
   ),
   1 => 
   array (
     'id' => 2,
     'name' => 'bar',
-    'password' => '$2y$10$1b54db67e9527d3549194OnbHhLODSb8K6s6Taw.yt8L3sCrhWaVi',
+    'password' => '$2y$10$Wrwi39JnXjCqDXx2XbRnweZrc8oC1xnhlxTzwJEnu1gwk3p.OSRJy',
   ),
   2 => 
   array (
     'id' => 3,
     'name' => 'baz',
-    'password' => '$2y$10$974ff147b45c746097d85eZRXSvAUsC6aFCXzNvMo4glTjxvQtcC2',
+    'password' => '$2y$10$wU1lRXQNQoefVbG/gs76hOyuwMfnxM1WCwomMNiV4yUnhmjQocAky',
   ),
   3 => 
   array (
     'id' => 4,
     'name' => 'me',
-    'password' => '$2y$10$ac80742c44c52f1224271ucQw/rrAt/lX79V.ACPsrVs2UwgsydHG',
+    'password' => '$2y$10$X9jUMxj6JkPtykSpMjPlqOOTNtFbOyUQpbayMwjsAl/ujFTrLPlOu',
   ),
   4 => 
   array (
     'name' => 'me2',
-    'password' => '$2y$10$a8faac678619d5e327817ey0G0rs7VRUMwOjs2nzF9fSVy6jcv6Ma',
+    'password' => '$2y$10$1sIwTjT3Fq6cnT/4xm6iu.bVAO7kbnSovx99.HomcVzJJZHRj9Uxm',
     'id' => 5,
   ),
 );
--- lib/password.php
+++ lib/password.php
@@ -1,15 +1,10 @@
 <?php
 
 function createPassword($password) {
-    $salt = md5(uniqid(rand(), true));
-    $raw = crypt($password, '$2y$10$' . $salt); 
-    if (strlen($raw) <= 13) {
-        throw new Exception('Crypt Failure!');
-    }
-    return $raw;
+    return password_hash($password, PASSWORD_DEFAULT);
 }
 
 function verifyPassword($hash, $password) {
-    return crypt($password, $hash) == $hash;
+    return password_verify($password, $hash);
 }
 
